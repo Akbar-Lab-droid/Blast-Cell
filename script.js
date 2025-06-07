65e27ae95476eb345f5de557fcb5c946dd1b0038
Akbar Ramadhan <jayasubang650@gmail.com>
2025-06-07 23:20:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll(".button");
-    buttons.forEach(button => {
-        button.addEventListener("mouseover", () => {
-            button.style.transform = "scale(1.1)";
-        });
-        button.addEventListener("mouseleave", () => {
-            button.style.transform = "scale(1)";
-        });
-    });
+document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
 });

936d11d07cffa17d1a5254b95cbe63ea3e55d535
Akbar Ramadhan <jayasubang650@gmail.com>
2025-06-07 23:27:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Hamburger Menu Toggle
 document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('active');
+});
+
+// Initialize AOS (Animate on Scroll)
+AOS.init({
+    duration: 1000,
+    once: true,
 });
